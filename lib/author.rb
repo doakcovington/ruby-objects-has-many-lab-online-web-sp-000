ca1f8ddaac0515fc1d0dfe9b625eadc7ea6c5c1d
Doak Covington <doakcovington@protonmail.com>
2020-02-13 03:21:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -12,7 +12,7 @@ class Author
     @posts = Post.all.select{|post| post.author == self}
   end
 
-  def add_post
+  def add_post(post)
     @posts << post
     post.author = self
     @@post_count += 1
